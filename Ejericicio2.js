b59d4385aeac92815bcc4248b465e341a934465a
GabrRott <gabrielroatta@gmail.com>
2022-09-23 09:40:27 -0300

    Creando variables

---
+++ Ejericicio2.js
@@ -0,0 +1,12 @@
+const nombre = 'Gabriel';
+const edad = 38;
+let soyDesarrollador = true;
+const fechaNacimiento = new Date(1984, 2, 17);
+var libroFav = {
+    titulo: 'El hobbit',
+    autor: 'J.R.R. Tolkien',
+    fecha: new Date(1950, 5, 22),
+    url: 'https://es.wikipedia.org/wiki/Hobbits'
+
+}
+
